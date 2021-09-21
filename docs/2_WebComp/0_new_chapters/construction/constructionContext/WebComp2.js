64bc4a8164fc328a0fb7ef41bf571aaafe1b294f
intertext.no <orstavik77@gmail.com>
2021-09-21 20:08:51 +0200

    simplifying

--- docs/2_WebComp/0_new_chapters/construction/constructionContext/WebComp2.js
+++ docs/2_WebComp/0_new_chapters/construction/constructionContext/WebComp2.js
@@ -5,38 +5,13 @@ function lastParsedElementInDocument() {
   return l;
 }
 
-function isSyncUpgrade(webComp, script) {
-  return !!(script && script.compareDocumentPosition(webComp) & Node.DOCUMENT_POSITION_CONTAINS);
-}
+const sequence = [];
 
-function predictiveRequire(context) {
-  return context.isLoading && !context.isCurrentScript;
-}
-
-function NEWrequire(context) {
-  return !context.hasParentNode && !context.cloneNode;
-}
-
-function isUpgrade(context) {
-  return context.currentScriptIsLastElement;
-}
-
-function isUpgradeWithin(context) {
-  return context.syncUpgrade;
-}
-
-function isInnerHTML(context) {
-  return context.isLoading && !context.isCurrentScript;
-}
-
-function isCloneNode(context) {
-  return context.cloneNode /*|| context.hasParentNode*/;
-}
-
-function analyzeContext(el) {
+window.log = function (el) {
   const lastElementInDocument = lastParsedElementInDocument();
   const currentScript = document.currentScript;
-  return {
+  const syncUpgrade = !!(currentScript && currentScript.compareDocumentPosition(el) & Node.DOCUMENT_POSITION_CONTAINS);
+  const data = {
     hasParentNode: !!el.parentNode,
     hasAttributes: !!el.attributes.length,
     hasChildNodes: !!el.childNodes.length,
@@ -46,21 +21,10 @@ function analyzeContext(el) {
     isEventListener: !!window.event,
     currentElementIsLastElement: lastElementInDocument === el,
     currentScriptIsLastElement: lastElementInDocument === currentScript,
-    syncUpgrade: isSyncUpgrade(el, currentScript),
-    cloneNode: cloneNode,
+    syncUpgrade,
   };
-}
-
-const sequence = [];
-
-window.log = function (el) {
-  const data = analyzeContext(el);
-  data.predictive = predictiveRequire(data);
-  data.NEW = !data.predictive && NEWrequire(data);
-  data.cloneNode = isCloneNode(data);
-  data.upgradeWithin = isUpgradeWithin(data);
-  data.upgrade = !data.upgradeWithin && isUpgrade(data);
-  data.innerHTML = !data.predictive && !data.cloneNode && !data.upgrade && isInnerHTML(data);
+  data.predictive = data.isLoading && !data.isCurrentScript;
+  data.NEW = !data.hasParentNode && !data.hasAttributes && !data.hasChildNodes;
   sequence.push(data);
 }
 
@@ -70,41 +34,11 @@ function print2() {
 
 setTimeout(print2, 100);
 
-let cloneNode = false;
-
 class WebComp extends HTMLElement {
   constructor() {
     super();
     log(this);
   }
-
-  //cloneNode if the element is the key of this cloneNode, then it might not have a parentNode.
-  cloneNode(deep) {
-    //todo don't know if it is better to check for this yet??
-    // if(!this.attributes.length && !this.childNodes.length && !this.parentNode)
-    //   return document.createElement(this.tagName);
-    // return Node.prototype.cloneNode.call(this, deep);
-    //during construction of completely shallow cloneNode calls, an otherwise empty '<clone-node-empty-element>' element
-    //is given as parentNode for the constructor so it can be separated from new Constructor() calls for the same type.
-    //if you read such a parentNode in the constructor, then you know that it is a cloneNode empty parentNode.
-    //the constructor() is context free. It doesn't know neither parent, attributes, nor children.
-    //the attributes are available during attributesReady.
-    //the parentNode is available at connectedCallback.
-    //the children are only available during childrenChangedCallback().
-    if (!this.attributes.length && (!this.childNodes.length || !deep)) {
-      const ghost = document.createElement('clone-node-empty-element');
-      ghost.innerHTML = `<${this.tagName}></${this.tagName}>`; //will create with a parentNode... This is not what you really get
-      const res = ghost.children[0];
-      res.remove();
-      return res;
-    }
-    return Node.prototype.cloneNode.call(this, deep);
-    // cloneNode = true;// !this.attributes.length && !this.childNodes.length && !this.parentNode;
-    // const res = Node.prototype.cloneNode.call(this, deep);
-    // cloneNode = false;
-    // return res;
-  }
 }
-
 
 customElements.define('web-comp', WebComp);
