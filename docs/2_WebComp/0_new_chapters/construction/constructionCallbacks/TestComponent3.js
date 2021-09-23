52babfdf35c0323eaca57c492aa18e58a977e63a
intertext.no <orstavik77@gmail.com>
2021-09-23 16:27:22 +0200

    ElementUid draft

--- docs/2_WebComp/0_new_chapters/construction/constructionCallbacks/TestComponent3.js
+++ docs/2_WebComp/0_new_chapters/construction/constructionCallbacks/TestComponent3.js
@@ -1,114 +1,3 @@
-//language=CSS
-const style = `
-  * {
-    --constructor: green;
-    --connected: blue;
-    --parent: blue;
-    --attribute: orange;
-    --slotchange: hotpink;
-    --child: hotpink;
-    --script: grey;
-    --defined: black;
-    --el1: url("data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' width='6' height='6'><rect fill='white' x='1.5' y='0' width='50%' height='100%'/></svg>");
-    --el2: url("data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' width='6' height='6'><circle fill='white' cx='3' cy='3' r='2'/></svg>");
-    --micro: 8px solid white;
-    --set: 50%;
-    --error: 4px solid red;
-  }
-
-  div.row > * {
-    box-sizing: border-box;
-    display: inline-block;
-    height: 20px;
-    width: 20px;
-    overflow: hidden;
-  }
-  div[name] {
-    width: 200px;
-    text-align: right;
-  }
-
-  .micro {
-    border-top: var(--micro);
-    border-right: var(--micro);
-  }
-  .set {
-    border-radius: var(--set);
-  }
-
-  .el1 {
-    background-image: var(--el1);
-  }
-  .el2 {
-    background-image: var(--el2);
-  }
-  .constructor {
-    background-color: var(--constructor);
-  }
-  .parent {
-    background-color: var(--parent);
-  }
-  .connected {
-    background-color: var(--connected);
-  }
-  .attribute {
-    background-color: var(--attribute);
-  }
-  .slotchange {
-    background-color: var(--slotchange);
-  }
-  .child {
-    background-color: var(--child);
-  }
-  .l1 {
-    border-bottom: var(--error);
-  }
-
-  .script {
-    background-color: var(--script);
-  }
-  .definition {
-    background-color: var(--defined);
-  }
-
-  .disconnected {
-    border-right: 3px solid red;
-  }
-  .empty {
-    border-left: 3px solid orange;
-  }
-  .innerHTML, .insertAdjacentHTML {
-    background-color: lightgrey;
-  }
-  .predictive {
-    background-color: pink;
-  }
-  .createElement, .NEW {
-    background-color: lightblue;
-  }
-  .cloneNode {
-    background-color: yellow;
-  }
-  .upgrade {
-    background-color: lightgreen;
-  }`;
-
-function findMissingActions(nowData, prevData) {
-  const res = [];
-  //1. add setAttribute multi/single
-  const addedAtts = nowData.attributesLength - prevData.attributesLength;
-  if (addedAtts)
-    res.push({id: nowData.id, name: 'set attribute l' + addedAtts});
-  //2. add setParentNode
-  if (nowData.hasParentNode !== prevData.hasParentNode)
-    res.push({id: nowData.id, name: 'set parent'});
-  //3. appendChild / appendChildren
-  const addedChildNodes = nowData.childNodesLength - prevData.childNodesLength;
-  if (addedChildNodes)
-    res.push({id: nowData.id, name: 'set child l' + addedChildNodes});
-  return res;
-}
-
 function analyze(sequence) {
   let res = [];
   for (let i = 0; i < sequence.length; i++) {
@@ -121,8 +10,18 @@ function analyze(sequence) {
         break;
       }
     }
-    const actions = findMissingActions(nowData, prevData);
-    res = [...res, ...actions, nowData];
+    //1. add setAttribute multi/single
+    const addedAtts = nowData.attributesLength - prevData.attributesLength;
+    if (addedAtts)
+      res.push({id: nowData.id, name: 'set attribute l' + addedAtts});
+    //2. add setParentNode
+    if (nowData.hasParentNode !== prevData.hasParentNode)
+      res.push({id: nowData.id, name: 'set parent'});
+    //3. appendChild / appendChildren
+    const addedChildNodes = nowData.childNodesLength - prevData.childNodesLength;
+    if (addedChildNodes)
+      res.push({id: nowData.id, name: 'set child l' + addedChildNodes});
+    res.push(nowData);
   }
   return res;
 }
@@ -130,24 +29,30 @@ function analyze(sequence) {
 export class TestHtml extends HTMLElement {
   static #counter = 0;
   #id;
+  #slot;
+  #iframe;
+  #div;
 
   constructor() {
     super();
     this.#id = TestHtml.#counter++;
     this.attachShadow({mode: "open"});
     this.shadowRoot.addEventListener('slotchange', e => this.slotchange(e));
-    this.shadowRoot.innerHTML = `<slot></slot><iframe hidden></iframe><style>${style}</style><div></div>`;
-    window.addEventListener('message', e => this.message(e));
+    this.shadowRoot.innerHTML = `<link rel="stylesheet" href="test.css"/><slot></slot><iframe hidden></iframe><div></div>`;
+    this.#slot = this.shadowRoot.children[1];
+    this.#iframe = this.shadowRoot.children[2];
+    this.#div = this.shadowRoot.children[3];
+    window.addEventListener('message', e=> this.onMessage(e));
   }
 
-  message(e) {
+  onMessage(e) {
     let res = JSON.parse(e.data);
-    if (res instanceof Array && res[0] === this.#id + '') 
+    if (res instanceof Array && res[0] === this.#id + '')
       this.render(analyze(res[1]));
   }
 
   render(ar) {
-    this.shadowRoot.querySelector('div').innerHTML = `
+    this.#div.innerHTML = `
 <div class="row">
     <div name class="${this.id}">${this.id}</div>
     ${ar.map(({name, id}) => `<div class="el${id} ${name}" title="${name}"></div>`).join('\n')}
@@ -155,15 +60,15 @@ export class TestHtml extends HTMLElement {
   }
 
   slotchange(e) {
-    const slotted = this.shadowRoot.children[0].assignedElements();
+    const slotted = this.#slot.assignedElements();
     if (!slotted.length)
-      return this.shadowRoot.children[2].innerHTML = "", this.shadowRoot.children[1].src = "";
+        return this.#div.innerHTML = "", this.#iframe.src = "";
     if (slotted.length > 1 || slotted[0].tagName !== 'NOSCRIPT')
       throw new Error('TestHtml can only contain a single <noscript></noscript> element.');
 
     const htmlText = slotted[0].innerHTML;
     this.setAttribute('title', htmlText);
-    this.shadowRoot.children[1].src =
+    this.#iframe.src =
       `data:text/html;charset=utf-8,${encodeURI(`<base href='${document.location.href}'/>${htmlText}`)}#${this.#id}`;
   }
 }
