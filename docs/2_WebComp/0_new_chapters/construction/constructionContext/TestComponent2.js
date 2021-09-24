386f42821b4455fb887cb11724cc29e649822639
intertext.no <orstavik77@gmail.com>
2021-09-24 15:24:16 +0200

    babysteps

--- docs/2_WebComp/0_new_chapters/construction/constructionContext/TestComponent2.js
+++ docs/2_WebComp/0_new_chapters/construction/constructionContext/TestComponent2.js
@@ -1,4 +1,4 @@
-export class TestHtml extends HTMLElement {
+class TestHtml extends HTMLElement {
   #id;
   #div;
   #slot;
@@ -9,11 +9,18 @@ export class TestHtml extends HTMLElement {
     super();
     this.attachShadow({mode: "open"});
     this.shadowRoot.addEventListener('slotchange', e => this.slotchange(e));
-    this.shadowRoot.innerHTML = `<slot></slot><iframe hidden></iframe><div table></div><link rel="stylesheet" href="test.css">`;
+    this.shadowRoot.innerHTML = `
+<slot></slot>
+<iframe hidden></iframe>
+<div table>
+  <div name class="${this.id}">${this.id}</div>
+  <div data></div>
+</div>
+<link rel="stylesheet" href="test.css">`;
     this.#id = this.id.replaceAll(' ', '');
     this.#slot = this.shadowRoot.children[0];
     this.#iframe = this.shadowRoot.children[1];
-    this.#div = this.shadowRoot.children[2];
+    this.#div = this.shadowRoot.children[2].children[1];
     window.addEventListener('message', e => this.onMessage(e));
   }
 
@@ -24,25 +31,37 @@ export class TestHtml extends HTMLElement {
   }
 
   render(res) {
-    const truth = Object.entries(res).filter(([k, v]) => v && k).map(([k,v])=>k);
+    const truth = Object.entries(res).filter(([k, v]) => v && k).map(([k, v]) => k);
     const row = Object.entries(res).map(([k, v]) => `<div class="${k}" value="${v}" title="${k}"> </div>`).join('\n');
-    this.#div.children[1].insertAdjacentHTML('beforeend', `<div class="row${this.#count++} ${truth.join(' ')}">${row}</div>`);
+    this.#div.insertAdjacentHTML('beforeend', `<div class="row${this.#count++} ${truth.join(' ')}">${row}</div>`);
   }
+
+  //todo set up so that I can choose which properties to compare
 
   slotchange(e) {
-    this.#div.innerHTML = this.#iframe.src = "";
     const slotted = this.#slot.assignedElements();
-    if (!slotted.length) return;
+    if (!slotted.length) return this.#div.innerHTML = this.#iframe.src = "";
     if (slotted.length > 1 || slotted[0].tagName !== 'NOSCRIPT')
       throw new Error('TestHtml can only contain a single <noscript></noscript> element.');
+    this.runTest();
+  }
 
-    const txt = `
-<base href='${document.location.href}'/>
-<script src='log.js'></script>
-<script src="window_constructionContext.js"></script>
-<script src="window_legalConstruction.js"></script>
-${slotted[0].innerHTML}`;
-    this.#div.insertAdjacentHTML('afterbegin', `<div name class="${this.id}">${this.id}</div><div data></div>`);
-    this.#iframe.src = `data:text/html;charset=utf-8,${encodeURI(`${txt}`)}#${this.#id}`;
+  runTest() {
+    this.#div.innerHTML = '';
+    const slotted = this.#slot.assignedElements();
+    const context = this.hasAttribute('context') ?'<script src="window_constructionContext.js"></script>':'';
+    const legal =this.hasAttribute('legal') ? '<script src="window_legalConstruction.js"></script>':'';
+    const txt = `<base href='${document.location.href}'/><script src='log.js'></script>${context}${legal}${slotted[0].innerHTML}`;
+    this.#iframe.src = `data:text/html;charset=utf-8,${encodeURI(txt)}#${this.#id}`;
+  }
+
+  attributeChangedCallback(){
+    this.runTest();
+  }
+
+  static get observedAttributes(){
+    return ['legal', 'context'];
   }
 }
+
+customElements.define('test-html', TestHtml);
