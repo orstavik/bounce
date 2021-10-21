e63db98425c9597c637a9b9238564c2be1fee0ab
intertext.no <orstavik77@gmail.com>
2021-10-21 10:38:49 +0200

    simplified tests:)

--- docs/2_WebComp/0_new_chapters/construction/test/log.js
+++ docs/2_WebComp/0_new_chapters/construction/test/log.js
@@ -1,6 +1,7 @@
 let count = 0;
 
 function customLog(...args) {
+  args.length === 1 && (args = args[0]);
   parent.postMessage(JSON.stringify(
     [location.hash.substr(1), args],
     (key, value) => value instanceof HTMLElement ? value.__id ??= count++ : value,
