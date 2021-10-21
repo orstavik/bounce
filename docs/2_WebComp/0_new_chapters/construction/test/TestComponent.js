4fe243dd3bde21f2e1f36abf659bd919269c19ad
intertext.no <orstavik77@gmail.com>
2021-10-21 13:59:29 +0200

    making something less annoying and something more annoying

--- docs/2_WebComp/0_new_chapters/construction/test/TestComponent.js
+++ docs/2_WebComp/0_new_chapters/construction/test/TestComponent.js
@@ -2,22 +2,34 @@
 const template = `
   <style>
     :host([ok="true"]) {
-      background: green;
+      border-left: 5px solid green;
     }
     :host([ok="false"]) {
-      background: red;
+      border-left: 5px solid red;
     }
     :host {
       display: block;
-      width: 100px;
-      height: 100px;
-      overflow: scroll;
+      /*width: 100px;*/
+      height: 50px;
+      overflow: hidden;
+    }
+    :host([active]) {
+      width: 90vw;
+      height: 90vh;
+    }
+    pre {
+      border: 2px dashed grey;
     }
   </style>
-  <pre id="result"></pre>
+  <h3></h3>
   <pre id="code"></pre>
+  <pre id="expected"></pre>
+  <pre id="result"></pre>
+  <pre id="diff"></pre>
   <iframe></iframe>
 `;
+
+import {} from "https://unpkg.com/diff@5.0.0/dist/diff.js";
 
 class TestHtml extends HTMLElement {
 
@@ -29,16 +41,23 @@ class TestHtml extends HTMLElement {
   #result;
   #code;
   #expected;
+  #diff;
+  #expectedShadow;
+  #title;
 
   constructor() {
     super();
     this.attachShadow({mode: "open"});
     this.shadowRoot.innerHTML = template;
     this.#expected = this.children[0];
+    this.#expectedShadow = this.shadowRoot.querySelector("#expected");
+    this.#title = this.shadowRoot.querySelector("h3");
     this.#result = this.shadowRoot.querySelector("#result");
     this.#code = this.shadowRoot.querySelector("#code");
+    this.#diff = this.shadowRoot.querySelector("#diff");
     this.#iframe = this.shadowRoot.querySelector("iframe");
     window.addEventListener('message', e => this.onMessage(e));
+    this.shadowRoot.addEventListener('click', e => this.onClick(e));
   }
 
   onMessage(e) {
@@ -50,14 +69,20 @@ class TestHtml extends HTMLElement {
   }
 
   render() {
+    this.#expectedShadow.textContent = this.#expected.textContent;
     this.#result.textContent = JSON.stringify(this.#resultObj, null, 3);
     const expect = JSON.stringify(JSON.parse(this.#expected.textContent), null, 3);
     this.setAttribute('ok', expect === this.#result.textContent);
   }
 
+  onClick() {
+    this.hasAttribute('active') ? this.removeAttribute('active') : this.setAttribute('active', '');
+  }
+
   async attributeChangedCallback(name, oldValue, newValue) {
     if (name === 'test') {
       //load the text content for the newValue of the test.
+      this.#title.textContent = newValue.substr(newValue.lastIndexOf('/') + 1);
       const testUrl = new URL(newValue, document.location);
       const logUrl = new URL('log.js', document.location);
       const response = await fetch(testUrl);
@@ -68,11 +93,13 @@ class TestHtml extends HTMLElement {
       console.log(newValue, data.length)
       // debugger
       this.#iframe.src = `data:text/html;charset=utf-8,${data}#${this.#id}`;
+    } else if (name === 'active' && newValue) {
+      this.#diff.textContent = Diff.diffWords(this.#expected.textContent, this.#result.textContent);
     }
   }
 
   static get observedAttributes() {
-    return ['test'];
+    return ['test', 'active'];
   }
 }
 
