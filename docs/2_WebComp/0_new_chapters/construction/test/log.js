5f706493f97bc684857f6ee0dd7779821d309543
intertext.no <orstavik77@gmail.com>
2021-10-29 17:39:54 +0200

    all green in FF and Chrome:)

--- docs/2_WebComp/0_new_chapters/construction/test/log.js
+++ docs/2_WebComp/0_new_chapters/construction/test/log.js
@@ -1,12 +1,16 @@
 let count = 0;
 
+function replacer(k, v) {
+  if (v instanceof HTMLElement)
+    return v.__id ??= count++;
+  if (v instanceof ErrorEvent)
+    return v.message.substr(v.message.indexOf(':') + 2);
+  return v;
+}
+
 function customLog(...args) {
   args.length === 1 && (args = args[0]);
-  parent.postMessage(JSON.stringify(
-    [location.hash.substr(1), args],
-    (key, value) => value instanceof HTMLElement ? value.__id ??= count++ : value instanceof ErrorEvent? value.message:  value,
-    2
-  ), '*');
+  parent.postMessage(JSON.stringify([location.hash.substr(1), args], replacer, 2), '*');
 }
 
 console.log = customLog;
