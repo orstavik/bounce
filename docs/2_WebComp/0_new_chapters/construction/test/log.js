e2cb2a5e47698729497d5bacc5a86e56925fad30
intertext.no <orstavik77@gmail.com>
2021-10-30 08:44:06 +0200

    babystep

--- docs/2_WebComp/0_new_chapters/construction/test/log.js
+++ docs/2_WebComp/0_new_chapters/construction/test/log.js
@@ -1,8 +1,6 @@
-let i = 0;
-
-const replacer = (k, v) => v instanceof HTMLElement ? (v.__id ??= i++) : v;
-
-console.log = function customLog(...args) {
-  args.length === 1 && (args = args[0]);
-  parent.postMessage(JSON.stringify([location.hash.substr(1), args], replacer, 2), '*');
-}
+console.log = (function (i = 0) {
+  return function customLog(...args) {
+    args.length === 1 && (args = args[0]);
+    parent.postMessage(JSON.stringify([location.hash.substr(1), args], (k, v) => v instanceof HTMLElement ? (v.__id ??= i++) : v, 2), '*');
+  }
+})();
