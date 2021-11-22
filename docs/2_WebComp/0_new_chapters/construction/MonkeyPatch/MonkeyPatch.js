3572d11aedb5481b03610f12c6def40737bf5fb1
intertext.no <orstavik77@gmail.com>
2021-11-22 13:24:03 +0100

    drafting MonkeyPatch

--- docs/2_WebComp/0_new_chapters/construction/MonkeyPatch/MonkeyPatch.js
+++ docs/2_WebComp/0_new_chapters/construction/MonkeyPatch/MonkeyPatch.js
@@ -23,6 +23,21 @@
       return og;
     }
 
+    //todo replace the monkeyPatchSetter with the monkeyPatchGetSet
+    static monkeyPatchGetSet(proto, prop, set, get) {
+      console.info("todo this is an untested method.");
+      const descriptor = Object.getOwnPropertyDescriptor(proto, prop);
+      const og = OG[proto.constructor.name + '.' + prop] = descriptor.get;
+      descriptor.get = function monkeypatch() {
+        return get.call(this, og);
+      };
+      descriptor.set = function monkeypatch() {
+        return set.call(this, og);
+      };
+      Object.defineProperty(proto, prop, descriptor);
+      return og;
+    }
+
     static dropClass(cnstr) {
       Object.setPrototypeOf(cnstr, Object.getPrototypeOf(Object.getPrototypeOf(cnstr)));
       Object.setPrototypeOf(cnstr.prototype, Object.getPrototypeOf(Object.getPrototypeOf(cnstr.prototype)));
@@ -42,15 +57,17 @@
       window.addEventListener('readystatechange', () => MonkeyPatch.dropClass(OG), {once: true, capture: true});
     }
 
-    static deprecate(proto, prop, msg){
+    static deprecate(proto, prop, msg) {
       const name = proto.constructor.name + "." + prop;
       function deprecate() {
         throw new SyntaxError(`"${name}" is deprecated. ${msg ?? ''}`);
       }
-      MonkeyPatch.monkeyPatch(proto, prop, deprecate);
+      const isGetSet = !Object.getOwnPropertyDescriptor(proto, prop).value;
+      isGetSet ? MonkeyPatch.monkeyPatchGetSet(proto, prop, deprecate, deprecate) :
+        MonkeyPatch.monkeyPatch(proto, prop, deprecate);
     }
 
-    static lockOG(name){
+    static lockOG(name) {
       const og = OG[name];
       delete OG[name];
       return og;
@@ -58,4 +75,4 @@
   }
 
   window.MonkeyPatch = MonkeyPatch;
-  })();
+})();
